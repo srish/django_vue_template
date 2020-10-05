4c273fb7f5ee77573c81dc9788c3e228f67e0a37
srish <srishakatux@gmail.com>
2020-10-05 00:03:03 -0700

    vue app files

---
+++ frontend/src/gallery.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './Gallery.vue'
+
+createApp(App).mount('#app')
---
+++ frontend/vue.config.js
@@ -0,0 +1,60 @@
+const BundleTracker = require("webpack-bundle-tracker");
+
+const pages = {
+    'home': {
+        entry: './src/home.js',
+        chunks: ['chunk-vendors']
+    },
+    'gallery': {
+        entry: './src/gallery.js',
+        chunks: ['chunk-vendors']
+    },
+}
+
+module.exports = {
+    pages: pages,
+    filenameHashing: false,
+    productionSourceMap: false,
+    publicPath: process.env.NODE_ENV === 'production'
+        ? ''
+        : 'http://localhost:8080/',
+    outputDir: '../build/static/',
+
+    chainWebpack: config => {
+
+        config.optimization
+            .splitChunks({
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name: "chunk-vendors",
+                        chunks: "all",
+                        priority: 1
+                    },
+                },
+            });
+
+        Object.keys(pages).forEach(page => {
+            config.plugins.delete(`html-${page}`);
+            config.plugins.delete(`preload-${page}`);
+            config.plugins.delete(`prefetch-${page}`);
+        })
+
+        config
+            .plugin('BundleTracker')
+            .use(BundleTracker, [{filename: '../frontend/webpack-stats.json'}]);
+
+        config.resolve.alias
+            .set('__STATIC__', 'static')
+
+        config.devServer
+            .public('http://localhost:8080')
+            .host('localhost')
+            .port(8080)
+            .hotOnly(true)
+            .watchOptions({poll: 1000})
+            .https(false)
+            .headers({"Access-Control-Allow-Origin": ["*"]})
+
+    }
+};
